6b1b37d2f8b61addbec8d9b0faa4b83af6c69928
al037805 <al037805@s09-pc04>
2014-01-10 12:40:37 +0100

    mantenimiento de Producto

---
+++ src/main/java/net/rafaelaznar/bean/ProductoBean.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.rafaelaznar.bean;
+
+/**
+ *
+ * @author al037805
+ */
+public class ProductoBean {
+    
+    private int id;
+    private String nombre;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
